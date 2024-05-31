04a1ebcdbcf72f7542d4950a7b7c0d7b12a37a8e
AMD <michojekunle1@gmail.com>
2024-05-31 07:35:36 +0100

    changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 const Button = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-    Get Started
-  </button>
+  <a
+    href="https://forms.gle/zaFPaWxWU1YE4poM9"
+    rel="noreferrer"
+    target="_blank"
+  >
+    <button
+      type="button"
+      className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  </a>
 );
 
 export default Button;
